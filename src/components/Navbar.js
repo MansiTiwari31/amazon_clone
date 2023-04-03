47704c3f19598513b87709cffab0a63e633a47ca
MansiTiwari31 <mansitiwari3103@gmail.com>
2023-04-03 14:07:15 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom';
+import logo from '../logo.svg';
+import jlnwc2tn from '../jlnwc2tn.png'
+import styled from 'styled-components';
+import {ButtonContainer} from './Button';
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <NavWrapper className="navbar nav-bar-expand-sm navbar-dark px-sm-5">
+               <Link to='/'>
+
+                  { <img src={jlnwc2tn} alt="store" className="navbar-brand" /> }
+               </Link>
+               <Link to='/'>
+
+                  { <img src={logo} alt="store" className="navbar-brands" /> }
+               </Link> 
+               <ul className="navbar-nav align-items-center">
+                 <li className="nav-item ml-5">
+                    <Link to="/" className="nav-link">
+                       Products
+                    </Link>
+                 </li>
+               </ul>
+               <Link to="/cart" className="ml-auto">
+                  <ButtonContainer>
+                      <i className="fas fa-cart-plus">my cart</i>
+                  </ButtonContainer>
+               </Link>
+            </NavWrapper>
+        )
+    }
+}
+const NavWrapper = styled.nav`
+background:var(--mainBlue);
+.nav-link{
+    color:var(--mainWhite) !important;
+    font-size:1.3 rem;
+    text-transform:capitalize;
+}
+
+
+`;
